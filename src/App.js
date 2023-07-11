3e10d2ab8db9940d5b33ef61f89a6179b1700668
patelmonik <patelmonik1110@gmail.com>
2023-07-11 08:34:39 +0530

    home_work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from '../src/component/Header';
+import Footer from '../src/component/Footer';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../src/container/Home';
+import Abount from '../src/container/About';
+import Events from '../src/container/Events';
 
 function App() {
   return (
-    
-  );
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/abount' element={<Abount />} />
+        <Route path='/events' element={<Events />} />
+      </Routes>
+      <Footer />
+    </>
+  )
 }
 
 export default App;
